ae71bfd6fcb6c7eda776ac8c414bfdea0ddbbcef
liyun <2549@163.com>
2022-04-23 11:00:19 +0800

    master student commit2

--- src/main/java/com/atuguigu/Sutent.java
+++ src/main/java/com/atuguigu/Sutent.java
@@ -5,5 +5,7 @@ public class Sutent {
         System.out.println("001");
         System.out.println("002");
         System.out.println("003");
+        System.out.println("master01");
+        System.out.println("master02");
     }
 }
